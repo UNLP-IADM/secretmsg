377a8711bc2b073130965fb11d0eb5972cba312d
GONZALO <gonzagaldamez@gmail.com>
2016-12-08 16:35:01 -0300

    New version

--- public/js/app3.js
+++ public/js/app3.js
@@ -1,16 +1,16 @@
 // MODULO CAMERA
 var camera = (function () {
   var picture = document.querySelector("#take-picture");
-  
-  var initCamera = function() {
+  var takenPicture = [];
+
+  function initCamera() {
     picture.onchange = function (event) {
-      var files = event.target.files,file;
+      var files = event.target.files, takenPicture;
       if (files && files.length > 0) {
-        file = files[0];
+        takenPicture = files[0];
       }
-      console.log('FOTO CARGADA');
-      picture.show();
-    };  
+      pictureReader.show(takenPicture);
+    };
   };
   
   return {
@@ -18,20 +18,71 @@ var camera = (function () {
       initCamera();
     }
   }
+  
 })();
 
-// MODULO PICTURE
-var picture = (function () {
-  var picture = document.querySelector("#show-picture");
-  var imgURL = window.URL.createObjectURL(file);
+// MODULO PICTUREREADER
+var pictureReader = (function () {
+  var showPicture = document.querySelector('#show-picture');
+  var fileReader = new FileReader();
+  
+  var readFile = function(pic) {
+    fileReader.onload = function (event) {
+        showPicture.src = event.target.result;
+    };
+    fileReader.readAsDataURL(pic);
+  } 
   
   return {
-    show: function() {
-      console.log('MOSTRANDO LA FOTO');
-      picture.src = imgURL;
+    show: function(pic) {
+      readFile(pic);
+      recognizer.decode(pic);
     }
   }
 })();
+
+
+// MODULO RECOGNIZER
+var recognizer = (function () {
+  
+  function decode_image (image) {
+    
+    var img = new Image();
+    img.src = document.querySelector('#show-picture').src;
+
+    var canvass = document.createElement('canvas');
+    canvass.width = 320;
+    canvass.height = 400;
+
+    var ctx = canvass.getContext('2d');
+    ctx.drawImage(img, 0, 0, canvass.width, canvass.height);
+
+    img.src = canvass.toDataURL("image/png");
+    image = img;  
+    
+
+    var canvas = document.createElement('canvas');
+    canvas.width = 326;
+    canvas.height = 214;
+    var context = canvas.getContext('2d');
+
+
+    context.drawImage(image, 0, 0);
+
+    var imageData = context.getImageData(0, 0, img.width, img.height);
+
+    var string = OCRAD(imageData);
+    var texto = document.querySelector('#message');
+    texto.innerHTML = string;
+  }
+  
+  return {
+    decode: function(image) {
+      decode_image(image);
+    }
+  }
+  
+})( );
 
 
 // MODULO GENERAL
